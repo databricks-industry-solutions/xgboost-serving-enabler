a6a70924042b64b1ecdbb91d3e6299f4df748e2b
dbbnicole <dbbnicole>
2023-05-23 22:31:18 +0000

    Update endpoint deployment util

--- util/create-update-serving-endpoint.py
+++ util/create-update-serving-endpoint.py
@@ -17,10 +17,10 @@ def endpoint_exists(serving_endpoint_name):
   response = requests.get(url, headers=headers)
   return response.status_code == 200
 
-def wait_for_endpoint():
+def wait_for_endpoint(serving_endpoint_name):
   """Wait until deployment is ready, then return endpoint config"""
   headers = { 'Authorization': f'Bearer {creds.token}' }
-  endpoint_url = f"https://{serving_host}/api/2.0/serving-endpoints/{config['serving_endpoint_name']}"
+  endpoint_url = f"https://{serving_host}/api/2.0/serving-endpoints/{serving_endpoint_name}"
   response = requests.request(method='GET', headers=headers, url=endpoint_url)
   while response.json()["state"]["ready"] == "NOT_READY" or response.json()["state"]["config_update"] == "IN_PROGRESS" : # if the endpoint isn't ready, or undergoing config update
     print("Waiting 30s for deployment or update to finish")
@@ -38,7 +38,7 @@ def create_endpoint(serving_endpoint_name, served_models):
   json_bytes = json.dumps(request_data).encode('utf-8')
   response = requests.post(endpoint_url, data=json_bytes, headers=headers)
   response.raise_for_status()
-  wait_for_endpoint()
+  wait_for_endpoint(serving_endpoint_name)
   displayHTML(f"""Created the <a href="/#mlflow/endpoints/{serving_endpoint_name}" target="_blank">{serving_endpoint_name}</a> serving endpoint""")
   
 def update_endpoint(serving_endpoint_name, served_models):
@@ -50,5 +50,5 @@ def update_endpoint(serving_endpoint_name, served_models):
   json_bytes = json.dumps(request_data).encode('utf-8')
   response = requests.put(endpoint_url, data=json_bytes, headers=headers)
   response.raise_for_status()
-  wait_for_endpoint()
+  wait_for_endpoint(serving_endpoint_name)
   displayHTML(f"""Updated the <a href="/#mlflow/endpoints/{serving_endpoint_name}" target="_blank">{serving_endpoint_name}</a> serving endpoint""")
